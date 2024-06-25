3ba0bbfc6817ec52aff30eba9dd0bc05636b842d
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-06-25 15:30:01 +0200

    build kernel on top of openapi

---
+++ src/ConducterSO/OnKernel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+namespace ConducterSO
+{
+    public static class OnKernel
+    {
+        public static Kernel BuildKernelOnTopOfOpenApi(IConfiguration configuration)
+        {
+            var openApiModel = configuration["OpenApi:model"];
+            var openApiKey = configuration["OpenApi:key"];
+
+            var builder = Kernel.CreateBuilder()
+                .AddOpenAIChatCompletion(openApiModel, openApiKey);
+
+            return builder.Build();
+        }
+    }
+}
--- src/ConducterSO/Program.cs
+++ src/ConducterSO/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConducterSO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var configurationBuilder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("config.json", optional: false, reloadOnChange: true);
+
+IConfiguration configuration = configurationBuilder.Build();
+
+OnKernel.BuildKernelOnTopOfOpenApi(configuration);
