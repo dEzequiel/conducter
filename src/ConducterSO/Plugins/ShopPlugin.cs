1e0e35663851fbfd3a5bb6298763073dd90f21fa
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-06-26 18:29:31 +0200

    create shop plugin to interact with products, discounts and baskets

--- src/ConducterSO/Plugins/ShopPlugin.cs
+++ src/ConducterSO/Plugins/ShopPlugin.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.SemanticKernel;
+﻿using ConducterSO.Models;
+using Microsoft.SemanticKernel;
 using System.ComponentModel;
 
-namespace ConducterSO.Models
+namespace ConducterSO.Plugins
 {
-    public class Shop
+    public class ShopPlugin
     {
         public string Name { get; private set; }
         public List<Product> _products { get; private set; } = new();
         private List<Basket> _baskets { get; set; } = new();
 
-        public Shop(string name)
+        public ShopPlugin(string name)
         {
             Name = name;
         }
@@ -17,42 +18,42 @@ namespace ConducterSO.Models
         [KernelFunction]
         public Basket? GetBasketByName(string name)
         {
-            return this._baskets.Find(x => x.Name == name);
+            return _baskets.Find(x => x.Name == name);
         }
 
         [KernelFunction("get_baskets")]
         [Description("Get baskets from the shop")]
         public List<Basket> GetBaskets()
         {
-            return this._baskets;
+            return _baskets;
         }
 
         [KernelFunction("get_products")]
         [Description("Get products from the shop")]
         public List<Product> GetProducts()
         {
-            return this._products;
+            return _products;
         }
 
         [KernelFunction("add_product_to_shop")]
         [Description("Add a new product to the shop catalog.")]
         public void AddProductToShop(Product product)
         {
-            this._products.Add(product);
+            _products.Add(product);
         }
 
         [KernelFunction("add_products_to_shop")]
         [Description("Add a collection of products to the shop catalog.")]
         public void AddProductsToShop(IEnumerable<Product> products)
         {
-            this._products.AddRange(products);
+            _products.AddRange(products);
         }
 
         [KernelFunction("add_baskets_to_shop")]
         [Description("Add a collection of baskets to the shop account.")]
         public void AddBasketsToShop(List<Basket> baskets)
         {
-            this._baskets.AddRange(baskets);
+            _baskets.AddRange(baskets);
         }
     }
 }
--- src/ConducterSO/Program.cs
+++ src/ConducterSO/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ConducterSO;
+using ConducterSO.Models;
+using ConducterSO.Plugins;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
@@ -14,4 +16,112 @@ var configurationBuilder = new ConfigurationBuilder().SetBasePath(AppContext.Bas
 
 IConfiguration configuration = configurationBuilder.Build();
 
-OnKernel.BuildKernelOnTopOfOpenApi(configuration);
+var openApiModel = configuration["OpenApi:model"];
+var openApiKey = configuration["OpenApi:key"];
+
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.AddOpenAIChatCompletion(openApiModel, openApiKey);
+
+var myShop = new ShopPlugin("My Shop");
+
+kernelBuilder.Plugins.AddFromType<LightsPlugin>("Lights");
+kernelBuilder.Plugins.AddFromType<TestDrivenDevelopmentPlugin>("TDD");
+kernelBuilder.Plugins.AddFromObject(myShop);
+
+var kernel = kernelBuilder.Build(); 
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Enable planning
+//OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+//{
+//    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+//};
+
+/**
+ * Enable automatic function calling
+ * Function calling is a way the AI can invoke functions with the corrects parameters.
+ */
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+var product1 = new Product("Laptop", 999.99m);
+var discountList1 = new List<Discount>
+{
+    new Discount(10.00m),
+    new Discount(15.50m)
+};
+product1.AddDiscounts(discountList1);
+
+var product2 = new Product("Smartphone", 499.49m);
+var discountList2 = new List<Discount>
+{
+    new Discount(20.00m),
+    new Discount(5.00m)
+};
+product2.AddDiscounts(discountList2);
+
+var product3 = new Product("Tablet", 299.99m);
+var discountList3 = new List<Discount>
+{
+    new Discount(7.25m),
+    new Discount(12.75m)
+};
+product3.AddDiscounts(discountList3);
+
+var product4 = new Product("Smartwatch", 199.99m);
+var discountList4 = new List<Discount>
+{
+    new Discount(18.00m),
+    new Discount(25.00m)
+};
+product4.AddDiscounts(discountList4);
+
+Basket basket1 = new Basket("Gifts for Family");
+basket1.AddProductToBasket(product1);
+var totalBasketPrice = basket1.SaveBasket();
+Console.WriteLine($"{basket1.Name} total price is {totalBasketPrice.ToString()}");
+
+Basket basket2 = new Basket("Gifts for Friends");
+basket2.AddProductToBasket(product1);
+totalBasketPrice = basket2.SaveBasket();
+Console.WriteLine($"{basket2.Name} total price is {totalBasketPrice.ToString()}");
+
+Basket basket3 = new Basket("Gifts for Pets");
+basket3.AddProductToBasket(product1);
+totalBasketPrice = basket3.SaveBasket();
+Console.WriteLine($"{basket3.Name} total price is {totalBasketPrice.ToString()}");
+
+myShop.AddBasketsToShop([basket1, basket2, basket3]);
+
+myShop.AddProductToShop(product1);
+myShop.AddProductToShop(product2);
+myShop.AddProductToShop(product3);
+myShop.AddProductToShop(product4);
+
+string? userInput;
+do
+{
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
