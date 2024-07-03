82c5dbcf0d7b124dfd9c5f8290028f1b49d7ac17
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-07-03 21:32:05 +0200

    register kernel clean

---
+++ src/ConducterSO/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using ConducterSO.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+
+namespace ConducterSO
+{
+    public static class Extensions
+    {
+        // https://github.com/microsoft/semantic-kernel/blob/main/dotnet/samples/Concepts/DependencyInjection/Kernel_Building.cs
+        public static void RegisterApplicationSemanticKernel(this HostApplicationBuilder builder)
+        {
+            var openApiModel = builder.Configuration["OpenApi:model"];
+            var openApiKey = builder.Configuration["OpenApi:key"];
+
+            builder.Services.AddTransient(srv =>
+            {
+                var builder = Kernel.CreateBuilder();
+                builder.RegisterKernelPlugins();
+                builder.Services.AddOpenAIChatCompletion(modelId: openApiModel, apiKey: openApiKey);
+                return builder.Build();
+            });
+
+            _ = builder.Services.BuildServiceProvider().GetRequiredService<Kernel>();
+            _ = builder.Services.BuildServiceProvider().GetRequiredService<ShopPlugin>();
+        }
+
+        private static void RegisterKernelPlugins(this IKernelBuilder builder)
+        {
+            builder.Plugins.AddFromType<LightsPlugin>("Lights");
+            builder.Plugins.AddFromType<ShopPlugin>("Shop");
+        }
+
+    }
+}
