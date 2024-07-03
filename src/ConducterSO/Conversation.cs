d15f15df159eefef672701993e04819a1e8a6b8b
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-07-03 22:07:14 +0200

    reorder

---
+++ src/ConducterSO/Conversation.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConducterSO
+{
+    public static class Conversation
+    {
+        /// <summary>
+        /// Starts a new chat session
+        /// </summary>
+        public async static Task StartChatAsync(this HostApplicationBuilder builder)
+        {
+            /**
+        * Enable automatic function calling
+        * Function calling is a way the AI can invoke functions with the corrects parameters.
+*/
+            OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+            {
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+            };
+
+            var srvProvider = builder.Services.BuildServiceProvider();
+            Console.WriteLine("Chat content:");
+            Console.WriteLine("------------------------");
+
+            // Create a history store the conversation
+            var history = new ChatHistory();
+
+            string? userInput;
+            var chatCompletitionService = srvProvider.GetRequiredService<IChatCompletionService>();
+            var kernelService = srvProvider.GetRequiredService<Kernel>();
+            do
+            {
+                // Collect user input
+                Console.Write("User > ");
+                userInput = Console.ReadLine();
+
+                // Add user input
+                history.AddUserMessage(userInput);
+
+                var result = await chatCompletitionService.GetChatMessageContentAsync(
+                    history,
+                    executionSettings: openAIPromptExecutionSettings,
+                    kernel: kernelService);
+
+                // Add the message from the agent to the chat history
+                history.AddMessage(result.Role, result.Content ?? string.Empty);
+
+                await MessageOutputAsync(history);
+            } while (userInput is not null);
+        }
+
+        /// <summary>
+        /// Outputs the last message of the chat history
+        /// </summary>
+        public static Task MessageOutputAsync(ChatHistory chatHistory)
+        {
+            var message = chatHistory.Last();
+
+            Console.WriteLine($"{message.Role} > {message.Content}");
+            Console.WriteLine("------------------------");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/ConducterSO/Program.cs
+++ src/ConducterSO/Program.cs
@@ -2,11 +2,6 @@
 using ConducterSO.Models;
 using ConducterSO.Plugins;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using System;
 using Microsoft.Extensions.Hosting;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
@@ -14,125 +9,73 @@ HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
 builder.Configuration.AddJsonFile("config.json", optional: false);
 builder.Configuration.AddCommandLine(args);
+builder.RegisterApplicationSemanticKernel();
 
-builder.AddKernel();
+Motion.Execute();
+await Conversation.StartChatAsync(builder);
 
-var serviceProvider = builder.Services.BuildServiceProvider();
+IHost host = builder.Build();
+host.Run();
 
-var product1 = new Product("Laptop", 999.99m);
-var discountList1 = new List<Discount>
+
+static class Motion
+{
+    public static void Execute()
+    {
+
+
+        var product1 = new Product("Laptop", 999.99m);
+        var discountList1 = new List<Discount>
 {
     new Discount(10.00m),
     new Discount(15.50m)
 };
-product1.AddDiscounts(discountList1);
+        product1.AddDiscounts(discountList1);
 
-var product2 = new Product("Smartphone", 499.49m);
-var discountList2 = new List<Discount>
+        var product2 = new Product("Smartphone", 499.49m);
+        var discountList2 = new List<Discount>
 {
     new Discount(20.00m),
     new Discount(5.00m)
 };
-product2.AddDiscounts(discountList2);
+        product2.AddDiscounts(discountList2);
 
-var product3 = new Product("Tablet", 299.99m);
-var discountList3 = new List<Discount>
+        var product3 = new Product("Tablet", 299.99m);
+        var discountList3 = new List<Discount>
 {
     new Discount(7.25m),
     new Discount(12.75m)
 };
-product3.AddDiscounts(discountList3);
+        product3.AddDiscounts(discountList3);
 
-var product4 = new Product("Smartwatch", 199.99m);
-var discountList4 = new List<Discount>
+        var product4 = new Product("Smartwatch", 199.99m);
+        var discountList4 = new List<Discount>
 {
     new Discount(18.00m),
     new Discount(25.00m)
 };
-product4.AddDiscounts(discountList4);
+        product4.AddDiscounts(discountList4);
 
-Basket basket1 = new Basket("Gifts for Family");
-basket1.AddProductToBasket(product1);
-var totalBasketPrice = basket1.SaveBasket();
-Console.WriteLine($"{basket1.Name} total price is {totalBasketPrice.ToString()}");
+        Basket basket1 = new Basket("Gifts for Family");
+        basket1.AddProductToBasket(product1);
+        var totalBasketPrice = basket1.SaveBasket();
+        Console.WriteLine($"{basket1.Name} total price is {totalBasketPrice.ToString()}");
 
-Basket basket2 = new Basket("Gifts for Friends");
-basket2.AddProductToBasket(product1);
-totalBasketPrice = basket2.SaveBasket();
-Console.WriteLine($"{basket2.Name} total price is {totalBasketPrice.ToString()}");
+        Basket basket2 = new Basket("Gifts for Friends");
+        basket2.AddProductToBasket(product1);
+        totalBasketPrice = basket2.SaveBasket();
+        Console.WriteLine($"{basket2.Name} total price is {totalBasketPrice.ToString()}");
 
-Basket basket3 = new Basket("Gifts for Pets");
-basket3.AddProductToBasket(product1);
-totalBasketPrice = basket3.SaveBasket();
-Console.WriteLine($"{basket3.Name} total price is {totalBasketPrice.ToString()}");
+        Basket basket3 = new Basket("Gifts for Pets");
+        basket3.AddProductToBasket(product1);
+        totalBasketPrice = basket3.SaveBasket();
+        Console.WriteLine($"{basket3.Name} total price is {totalBasketPrice.ToString()}");
 
-//myShop.AddBasketsToShop([basket1, basket2, basket3]);
-
-//myShop.AddProductToShop(product1);
-//myShop.AddProductToShop(product2);
-//myShop.AddProductToShop(product3);
-//myShop.AddProductToShop(product4);
-
-await StartChatAsync();
-
-IHost host = builder.Build();
-
-/// <summary>
-/// Starts a new chat session
-/// </summary>
-async Task StartChatAsync()
-{
-    /**
-* Enable automatic function calling
-* Function calling is a way the AI can invoke functions with the corrects parameters.
-*/
-    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
-    {
-        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
-    };
-
-
-    Console.WriteLine("Chat content:");
-    Console.WriteLine("------------------------");
-
-    // Create a history store the conversation
-    var history = new ChatHistory();
-
-    string? userInput;
-    var chatCompletitionService = serviceProvider.GetRequiredService<IChatCompletionService>();
-    var kernelService = serviceProvider.GetRequiredService<Kernel>();
-    do
-    {
-        // Collect user input
-        Console.Write("User > ");
-        userInput = Console.ReadLine();
-
-        // Add user input
-        history.AddUserMessage(userInput);
-
-        var result = await chatCompletitionService.GetChatMessageContentAsync(
-            history,
-            executionSettings: openAIPromptExecutionSettings,
-            kernel: kernelService);
-
-        // Add the message from the agent to the chat history
-        history.AddMessage(result.Role, result.Content ?? string.Empty);
-        
-        await MessageOutputAsync(history);
-    } while (userInput is not null);
+        var myShop = new ShopPlugin();
+        myShop.AddBasketsToShop([basket1, basket2, basket3]);
+        myShop.AddProductToShop(product1);
+        myShop.AddProductToShop(product2);
+        myShop.AddProductToShop(product3);
+        myShop.AddProductToShop(product4);
+    }
 }
-/// <summary>
-/// Outputs the last message of the chat history
-/// </summary>
-Task MessageOutputAsync(ChatHistory chatHistory)
-{
-    var message = chatHistory.Last();
-
-    Console.WriteLine($"{message.Role} > {message.Content}");
-    Console.WriteLine("------------------------");
-
-    return Task.CompletedTask;
-}
-
-host.Run();
-
